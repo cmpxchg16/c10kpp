0246497f5efe1aa9ff63822773a28ed1d76e06f4
Shachar Shemesh <shachar@shemesh.biz>
2013-11-09 22:22:11 +0200

    Clarify license (GPL)

--- asio-http-server/src/server.cpp
+++ asio-http-server/src/server.cpp
@@ -1,3 +1,22 @@
+/*
+    C10KPP - Proof of concept 10K concurrent clients
+    Copyright (C) 2013 Uri Shamay and Shachar Shemesh
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+ */
 #include "server.hpp"
 #include <boost/bind.hpp>
 
